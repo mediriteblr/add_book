4b90b10a7daae6bc0c07e7df48c31d064ec154a3
Kiran Kumar K B <kiran.kumar@aptos.com>
2024-12-11 12:22:13 +0530

    changed server-script shell file

--- server-script.sh
+++ server-script.sh
@@ -8,12 +8,12 @@ if [ -d "add_book" ]
 then
   echo "repo is cloned and exists"
     git pull origin docker-nov
-    cd add_book-v1
+    cd add_book
 else
   git clone https://github.com/mediriteblr/add_book.git
 fi
 
-cd add_book-v1
+cd add_book
 # git checkout docker-nov
 # mvn package
 # mvn -U deploy -s settings.xml
