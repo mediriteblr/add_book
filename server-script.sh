2051d768d79f670854dee63aca6783cc8b7a2fad
Kiran Kumar K B <kiran.kumar@aptos.com>
2024-12-11 12:10:37 +0530

    changed server-script shell file

--- server-script.sh
+++ server-script.sh
@@ -4,16 +4,16 @@ sudo yum install git -y
 sudo yum install docker -y
 sudo systemctl start docker
 
-if [ -d "addressbook-v1" ]
+if [ -d "addbook-v1" ]
 then
   echo "repo is cloned and exists"
     git pull origin docker-nov
-    cd addressbook-v1
+    cd addbook-v1
 else
   git clone https://github.com/mediriteblr/add_book.git
 fi
 
-cd addressbook-v1
+cd addbook-v1
 # git checkout docker-nov
 # mvn package
 # mvn -U deploy -s settings.xml
